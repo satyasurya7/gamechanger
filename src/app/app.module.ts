97edb6ef56669d7089eb2ccb84db5e21ea21db0f
Narendra Yechuri <satyasurya7@gmail.com>
2022-07-20 23:37:14 +0530

    Minior Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppAboutComponent } from './app-about/app-about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppAboutComponent
   ],
   imports: [
     BrowserModule,
